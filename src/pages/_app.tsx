5d49b44f19acf885103eebee2391da36e467bb44
lyrarod <lyrarod@gmail.com>
2022-09-15 09:13:38 -0300

    upload vercel error (3)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
-import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { theme } from '../styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps }): JSX.Element {
   const queryClient = new QueryClient();
 
   return (
